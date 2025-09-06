8f835f8754ccc1685cd9c5f0696f35b9e5238e3b
Atish Adhav <adhavatish04@gmail.com>
2025-09-06 18:06:05 +0530

    changes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 
 /* Routes */
@@ -10,8 +11,6 @@ const chatRoutes = require("./routes/chat.routes");
 
 const app = express();
 
-
-
 /* using middlewares */
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -19,12 +18,17 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 
 /* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 
 
